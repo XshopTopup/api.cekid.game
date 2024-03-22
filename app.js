7e72775d01d90116702c18138ec99dc77729fe91
Andrian-Fh <98972143+XshopTopup@users.noreply.github.com>
2024-03-23 03:34:11 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-case 'higgsid':
-    case 'dominoid':
+case 'cekpln':
 if (!isGroup) return replyDeface(mess.OnlyGrup)
 if (!q) return replyDeface(`Gunakan dengan cara ${prefix + command} *id*\n\n_Contoh_\n\n${command} 513429887`)
-axios.get(`https://v1.apigames.id/merchant/M220515ALZM2045XZ/cek-username/higgs?user_id=${q}&signature=6ffbc2d3102edf02c5482bac8c5ab9da`)
+axios.get(`curl --location 'https://api.sqpayment.my.id/v3/game/pln/?nomor=32021963072&key=xxxxx'`)
 .then(({data}) => {
-let domino = `*🔎 HIGGS DOMINO 🔍*
+let domino = `*🔎 PLN 🔍*
 
 ID : ${q}
 Nick : ${data.data.username}`
-replyDeface(domino)
+replyDeface(pln)
 })
 .catch((err) => {
 console.log(color('[ ERROR ]', 'red'), err)
 replyDeface(mess.error.api)
-saipul.sendMessage(ownerNumber, { text: `${command} error : ${err}` })
+xshoptopup.sendMessage(ownerNumber, { text: `${command} error : ${err}` })
 })
 break
