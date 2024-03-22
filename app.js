9806b93b9352d0f0337b5a7d7952ee50d8a4dd83
Andrian-Fh <98972143+XshopTopup@users.noreply.github.com>
2024-03-23 03:38:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 case 'cekpln':
 if (!isGroup) return replyDeface(mess.OnlyGrup)
 if (!q) return replyDeface(`Gunakan dengan cara ${prefix + command} *id*\n\n_Contoh_\n\n${command} 513429887`)
-axios.get(`curl --location 'https://api.sqpayment.my.id/v3/game/pln/?nomor=32021963072&key=xxxxx'`)
+axios.get(`curl --location 'https://api.sqpayment.my.id/v3/game/pln/${q}?apikey=${apikey}'`)
 .then(({data}) => {
 let domino = `*🔎 PLN 🔍*
 
